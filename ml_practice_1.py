3dc7f96f765b0f20441d154814db3b5936a0c04e
Theekshana Vidushan <mahesh190290390@gmail.com>
2025-10-08 10:18:10 +0530

    first

---
+++ ml_practice_1.py
@@ -0,0 +1,27 @@
+full_country_stats.plot(kind='scatter', figsize=(8, 3),
+                        x=gdppc_col, y=lifesat_col, grid=True)
+
+for country, pos_text in position_text_missing_countries.items():
+    pos_data_x, pos_data_y = missing_data.loc[country]
+    plt.annotate(country, xy=(pos_data_x, pos_data_y),
+                 xytext=pos_text, fontsize=12,
+                 arrowprops=dict(facecolor='black', width=0.5,
+                                 shrink=0.08, headwidth=5))
+    plt.plot(pos_data_x, pos_data_y, "rs")
+
+X = np.linspace(0, 115_000, 1000)
+plt.plot(X, t0 + t1 * X, "b:")
+
+lin_reg_full = linear_model.LinearRegression()
+Xfull = np.c_[full_country_stats[gdppc_col]]
+yfull = np.c_[full_country_stats[lifesat_col]]
+lin_reg_full.fit(Xfull, yfull)
+
+t0full, t1full = lin_reg_full.intercept_[0], lin_reg_full.coef_.ravel()[0]
+X = np.linspace(0, 115_000, 1000)
+plt.plot(X, t0full + t1full * X, "k")
+
+plt.axis([0, 115_000, min_life_sat, max_life_sat])
+
+save_fig('representative_training_data_scatterplot')
+plt.show()
