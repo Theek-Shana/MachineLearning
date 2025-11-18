ca5d169a79b78b6f19575bde6570dd5a5ca0a3e6
Theekshana Vidushan <mahesh190290390@gmail.com>
2025-11-18 19:51:29 +0530

    Update gest.py

--- gest.py
+++ gest.py
@@ -18,7 +18,7 @@ rnd_clf.fit(X_train, y_train)
 
 # 5️⃣ Make predictions
 y_pred = rnd_clf.predict(X_test)
-
+ 
 # 6️⃣ Check accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Random Forest accuracy on MNIST: {accuracy:.4f}")
@@ -33,4 +33,5 @@ plt.imshow(image, cmap="gray")
 plt.title(f"Predicted: {rnd_clf.predict([X_test[some_index]])[0]}")
 plt.axis("off")
 plt.show()
+
 
