6b542ec0c2d47c5ebfcd5298f3093f8b784a247f
Theekshana Vidushan <mahesh190290390@gmail.com>
2026-01-22 21:44:31 +0530

    Reformat ml_practice_20.py for consistency

--- ml_practice_20.py
+++ ml_practice_20.py
@@ -12,7 +12,7 @@ X = np.random.randn(200, 2)
 kmeans = KMeans(n_clusters=3, random_state=42)
 y_pred = kmeans.fit_predict(X)
 
-# Plot the clusters 
+# Plot the clusters  
 plt.scatter(X[:, 0], X[:, 1], c=y_pred, cmap="viridis", s=50)
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
             c="red", marker="X", s=200, label="Centroids")
@@ -20,6 +20,7 @@ plt.title("K-Means Clustering Example")
 plt.legend() 
 
 plt.show()
+
 
 
 
