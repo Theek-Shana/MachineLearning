a576f709a1bde1e6bf966148d2a317f627e668b1
Theekshana Vidushan <mahesh190290390@gmail.com>
2026-01-22 21:44:03 +0530

    Update ml_practice_2.py

--- ml_practice_2.py
+++ ml_practice_2.py
@@ -17,7 +17,7 @@ y = df['Marks']
 
 # Step 4: Split for training & testing (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+ 
 # Step 5: Create and train model
 model = LinearRegression()
 model.fit(X_train, y_train)
@@ -33,4 +33,5 @@ print("Mean Squared Error:", mse)
 hours = float(input("Enter study hours: "))
 predicted_marks = model.predict([[hours]])
 print(f"Predicted Marks for {hours} hours = {predicted_marks[0]:.2f}")
+
 
